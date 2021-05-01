dbb6fc940dee2550713f76b7ec9f294169a40a98
nvizcarra <nicolasvizcarra64@gmail.com>
2021-05-01 15:47:02 -0300

    Destructuring exercise

---
+++ js/destructuring.js
@@ -0,0 +1,21 @@
+// Destructuring de objetos
+const aprendiendoJS = {
+    version: {
+        nueva : 'ES6',
+        anterior : 'ES5'
+    },
+    frameworks: ['React', 'VueJS', 'Angular']
+}
+
+// console.log(aprendiendoJS);
+
+// Forma anterior
+
+// let version = aprendiendoJS.version.nueva;
+// let framework = aprendiendoJS.frameworks[1];
+
+// Forma nueva
+
+let {version, frameworks} = aprendiendoJS;
+console.log(version);
+console.log(frameworks);
