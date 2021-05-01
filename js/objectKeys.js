c883daab78b51fa72c2f62687d45e853ec8d32ea
nvizcarra <nicolasvizcarra64@gmail.com>
2021-05-01 18:54:26 -0300

    Object keys exercise

---
+++ js/objectKeys.js
@@ -0,0 +1,8 @@
+const  persona = {
+    nombre: 'Nicolás',
+    profesion: 'desarrollador web',
+    edad: 30
+}
+
+// Traer nombre, profesion y edad mediante Object keys
+console.log(Object.keys(persona));
