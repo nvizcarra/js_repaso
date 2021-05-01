6cc832b5a237f92bcc4e7bb206056468ef0c18c8
nvizcarra <nicolasvizcarra64@gmail.com>
2021-05-01 17:31:04 -0300

    Object Literal Enhancement Exercise

---
+++ js/objectLiteralEnhancement.js
@@ -0,0 +1,8 @@
+const banda = 'Metallica';
+const genero = 'Heavy Metal';
+const canciones = ['Master of Puppets', 'Seek and Destroy', 'Enter the Sandman'];
+
+
+const metallica = {banda, genero, canciones};
+
+console.log(metallica);
